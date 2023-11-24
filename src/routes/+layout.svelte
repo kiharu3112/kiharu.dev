ced947f004eea1f50a42eccce9d73f2248acb5e3
kiharu3112 <74781355+kiharu3112@users.noreply.github.com>
2023-11-25 08:35:21 +0900

    header and footer and font

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,45 @@
+<script>
+    import { page } from "$app/stores";
+</script>
+<header>
+  <h1><a href="/">kiharu.dev</a></h1>
+  <a href="https://github.com/kiharu3112" target="_blank" rel="noopener noreferrer"><img src="github-mark.svg" alt="GitHub" /></a>
+</header>
+<div id="content">
+  <slot/>
+</div>
+
+<footer>
+  <p>© 2023 kiharu.dev</p>
+</footer>
+
+<style>
+    header {
+        margin: 0;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        background: #eee;
+        height: 3rem;
+        color: black;
+    }
+    header a {
+        margin: 0 1rem;
+        text-decoration: none;
+        color: black;
+        font-weight: bold;
+    }
+    header img {
+        height: 1.5rem;
+        margin-right: 0.5rem;
+    }
+    #content {
+        margin: 0 auto;
+        padding: 1rem;
+    }
+    footer {
+        margin-top: 2rem;
+        text-align: center;
+        color: #999;
+    }
+</style>
