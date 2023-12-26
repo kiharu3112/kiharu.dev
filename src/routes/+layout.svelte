a1087291680c8a5dc694721fec34ff38cc4e05b4
kiharu3112 <74781355+kiharu3112@users.noreply.github.com>
2023-12-26 12:55:05 +0900

    更新

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,60 +1,59 @@
-<script>
-    import { page } from "$app/stores";
-</script>
 <header>
-  <div class="content-header">
-    <h1><a href="/">kiharu.dev</a></h1>
-    <a href="https://github.com/kiharu3112" target="_blank" rel="noopener noreferrer"><img src="github-mark.svg" alt="GitHub" /></a>
-  </div>
+	<div class="content-header">
+		<h1><a href="/">*******</a></h1>
+	</div>
 </header>
 <div class="content">
-  <slot/>
+	<slot />
 </div>
 
 <footer>
-  <p>© 2023 kiharu.dev</p>
+	<p>© 2023 kiharu.dev</p>
 </footer>
 
 <style>
-    header {
-        margin: 0;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        background: #eee;
-        height: 3rem;
-        color: black;
-    }
-    header a {
-        margin: 0 1rem;
-        text-decoration: none;
-        color: black;
-        font-weight: bold;
-    }
-    header img {
-        height: 1.5rem;
-        margin-right: 0.5rem;
-    }
-    .content-header {
-        margin: 0 auto;
-        padding: 0;
-        width: 80%;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-    }
-    header .content-header h1 a {
-        padding: 0;
-        margin: 0;
-    }
-    footer {
-        margin-top: 2rem;
-        text-align: center;
-        color: #999;
-    }
-    .content {
-        margin: 0 auto;
-        padding: 0;
-        width: 80%;
-    }
+	:global(body) {
+		margin: 0;
+		padding: 0;
+		background-color: #333;
+	}
+	header {
+		margin: 0;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		height: 3rem;
+		color: #ddd;
+		background-color: #5e5184;
+	}
+	header a {
+		margin: 0 1rem;
+		text-decoration: none;
+		color: #ddd;
+		font-weight: bold;
+	}
+	.content-header {
+		margin: 0 auto;
+		padding: 0;
+		width: 80%;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+	header .content-header h1 a {
+		padding: 0;
+		margin: 0;
+	}
+	footer {
+		margin-top: 2rem;
+		text-align: center;
+		color: #999;
+	}
+	.content {
+		margin: 0 auto;
+		padding: 0;
+		width: 80%;
+		background-color: #333;
+		color: #fefefe;
+	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,45 +1,127 @@
-<h1>Hello World</h1>
-<h2>profile</h2>
-<p>name: kiharu</p>
-<p>age: 18</p>
-<p>NITMC</p>
-<h2>hobby</h2>
-<p>programming</p>
-<p>music</p>
-<p>game</p>
-<h2>works</h2>
-<h2>life</h2>
+<div id="profile">
+	<img src="https://github.com/kiharu3112.png" alt="me icon" id="icon" />
+	<div>
+		<h1>プロフィール</h1>
+		<ul>
+			<li>Name: Haruto Kijima</li>
+			<li>Age: 18</li>
+			<li>School: NITMC</li>
+			<li>github: <a href="https://github.com/kiharu3112">kiharu3112</a></li>
+		</ul>
+	</div>
+</div>
+<h2>趣味</h2>
+<ul>
+	<li>pyogramming</li>
+	<li>listen music(YouTube, SoundCloud, ...)</li>
+	<li>play game</li>
+</ul>
+
+<h2>創作物</h2>
+<h3>WashBoard</h3>
+<div class="works">
+	<img
+		src="https://flashy-buckaroo-213.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2F807403ae-affd-476f-95db-85cc30024b58%2F0d152d9f-0936-4641-b4e5-3b25e00da10a%2Ficon-144x144.png?table=block&id=eaf21e1b-a96a-4581-8a58-45808a48ddc3&spaceId=807403ae-affd-476f-95db-85cc30024b58&width=250&userId=&cache=v2"
+		alt="WashBoardのアイコン"
+		width="80px"
+		height="80px"
+	/>
+	<ul>
+		<li>学生寮の洗濯管理をスマートに</li>
+		<li>
+			<a href="https://flashy-buckaroo-213.notion.site/WashBoard-eaf21e1ba96a45818a5845808a48ddc3"
+				>紹介ページ</a
+			>
+		</li>
+		<li>
+			<a href="https://washboard.pages.dev">アプリ</a>
+		</li>
+		<li>
+			<a href="https://github.com/mct-joken/washboard">ソースコード</a>
+		</li>
+	</ul>
+</div>
+
+<h2>人生</h2>
 <table>
-  <th>year</th>
-  <th>event</th>
-  <tr>
-    <td>2004</td>
-    <td>born</td>
-  </tr>
-  <tr>
-    <td>2020</td>
-    <td>enter NITMC</td>
-  </tr>
-  <tr>
-    <td>2023</td>
-    <td>joined Computer Festival</td>
-  </tr>
-  <tr>
-    <td>2023</td>
-    <td>joined procon34</td>
-  </tr>
-  <tr>
-    <td>2024</td>
-    <td>something seems to be about to happen</td>
-  </tr>
+	<th>year</th>
+	<th>event</th>
+	<tr>
+		<td>2004</td>
+		<td>生誕</td>
+	</tr>
+	<tr>
+		<td>2020</td>
+		<td><a href="https://www.matsue-ct.jp/" target="_blank">NITMatsue Colledge</a>入学</td>
+	</tr>
+	<tr>
+		<td>2022</td>
+		<td><a href="https://joken.dev" target="_blank">情報科学研究部</a>入部</td>
+	</tr>
+	<tr>
+		<td>2023</td>
+		<td>中国地区高専コンピューターフェスティバル参加</td>
+	</tr>
+	<tr>
+		<td>2023</td>
+		<td><a href="https://www.procon.gr.jp/?page_id=78840" target="_blank">#procon34</a>参加</td>
+	</tr>
+	<tr>
+		<td>2023</td>
+		<td
+			><a href="https://2023.rubyworld-conf.org/ja/" target="_blank">Ruby World Conference 2023</a
+			>参加</td
+		>
+	</tr>
+	<tr>
+		<td>2024~</td>
+		<td>なにかが起きるかもしれない！？</td>
+	</tr>
 </table>
 
 <style>
-  table {
-    border-collapse: collapse;
-  }
-  th, td {
-    border: 1px solid black;
-    padding: 0.5rem;
-  }
+	#profile {
+    display: flex;
+		margin: 2rem 0;
+    width: 100%;
+		max-width: 35rem;
+    flex-wrap: wrap;
+	}
+	#icon {
+		width: 200px;
+		height: 200px;
+		border-radius: 50%;
+		display: block;
+	}
+	ul {
+		padding: 0 2rem;
+		margin: 0;
+	}
+	li {
+		margin: 0.5rem 0;
+	}
+	h1,
+	h2,
+	h3 {
+		color: #bca2ff;
+	}
+	a {
+		color: #eb82dc;
+	}
+	table {
+		border-collapse: collapse;
+		border: 1px solid #fefefe;
+	}
+	th,
+	td {
+		text-align: left;
+		border: 1px solid #fefefe;
+		padding: 1rem;
+		border-right: 0;
+		border-left: 0;
+	}
+	.works {
+		display: flex;
+		align-items: center;
+	}
 </style>
